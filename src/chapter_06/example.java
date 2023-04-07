c3d3809f5a2e1dd24190ad24e2d0210185b3ecad
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-04-07 06:47:46 +0000

    first commit

---
+++ src/chapter_06/example.java
@@ -0,0 +1,13 @@
+package chapter_06;
+
+public class example {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello goorm!");
+    }
+
+}
