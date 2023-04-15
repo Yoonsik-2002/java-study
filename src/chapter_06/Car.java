ae410005551cb2a3cf39982164a0afc2a45f153b
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-04-15 01:31:23 +0000

    add Car.java

---
+++ src/chapter_06/Car.java
@@ -0,0 +1,11 @@
+package chapter_06;
+
+public class Car {
+    String name;
+    String price;
+    
+    public Car(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+}
