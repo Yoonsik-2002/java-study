a21ce4da75de0e977b06cfba24300c66f0752b77
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-04-15 05:43:03 +0000

    array of objects - saving objects using for statements

--- src/chapter_06/Ex06_5.java
+++ src/chapter_06/Ex06_5.java
@@ -19,7 +19,7 @@ public class Ex06_5{
         }
         
         // car_arr에 저장된 내용 출력
-        System.out.println("----- List of cars -----");
+        System.out.println("------ List of cars ------");
         for (int i = 0; i < car_arr.length; i++) {
             System.out.println("[" + car_arr[i].name + " , " + car_arr[i].price + "]");
         }
