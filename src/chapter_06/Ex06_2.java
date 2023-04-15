26610fd5f1a765d68f18ae5f7ff92e955bdf307e
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-04-15 05:54:51 +0000

    creating and using objects - example_02

--- src/chapter_06/Ex06_2.java
+++ src/chapter_06/Ex06_2.java
@@ -5,14 +5,14 @@ class Ex06_2 {
         Tv tv1 = new Tv();
         Tv tv2 = new Tv();
         
-        System.out.println("tv1 의 채널값은" + tv1.channel + "입니다.");
-        System.out.println("tv2 의 채널값은" + tv2.channel + "입니다.");
+        System.out.println("tv1 의 채널값은 " + tv1.channel + " 입니다.");
+        System.out.println("tv2 의 채널값은 " + tv2.channel + " 입니다.");
         
         tv1.channel = 7;
         System.out.println("tv1 의 채널값을 7로 변경하였습니다.");
         
-        System.out.println("tv1 의 채널값은" + tv1.channel + "입니다.");
-        System.out.println("tv2 의 채널값은" + tv2.channel + "입니다.");
-    }
+        System.out.println("tv1 의 채널값은 " + tv1.channel + " 입니다.");
+        System.out.println("tv2 의 채널값은 " + tv2.channel + " 입니다.");
+    } 
 }
     
