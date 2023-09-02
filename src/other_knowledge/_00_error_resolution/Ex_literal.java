6fdc8c8c30e94befdb9962253d553d8d4a6ff9b4
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-02 09:43:21 +0000

    modify Ex_literal.java

--- src/other_knowledge/_00_error_resolution/Ex_literal.java
+++ src/other_knowledge/_00_error_resolution/Ex_literal.java
@@ -2,7 +2,7 @@ package other_knowledge._00_error_resolution;
 
 public class Ex_literal {
     public static void main(String[] args) {
-        long ex_long = 12345678912;
-        float ex_float = 1199.80;
+        long ex_long = 12345678912L;
+        float ex_float = 1199.80F;
     }
 }
