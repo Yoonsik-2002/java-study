07d0927b145dbee40db154075e7c615c105b3813
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-11 10:06:22 +0000

    chapter_06 Exercise - Exercise6_3.java

--- src/_01_Exercise/_00_chapter_06/Exercise6_3.java
+++ src/_01_Exercise/_00_chapter_06/Exercise6_3.java
@@ -2,8 +2,8 @@ package _01_Exercise._00_chapter_06;
 
 public class Exercise6_3 {
     static double getDistance(int x, int y, int x1, int y1) {
-        double distance = Math.pow(x1 - x, 2) + Math.pow(y1 - y, 2);
-        return Math.sqrt(distance);
+        double distance = Math.pow(x1 - x, 2) + Math.pow(y1 - y, 2); // 특정 값의 제곱을 구해주는 java.lang 패키지의 Math.pow()메서드
+        return Math.sqrt(distance); // 특정 값의 제곱근을 구해주는 java.lang 패키지의 Math.sqrt()메서드
     }
     
     public static void main(String[] args) {
