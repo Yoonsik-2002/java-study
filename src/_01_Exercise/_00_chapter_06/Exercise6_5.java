e608455848fd9b26171059ba6b2f55ad939525d6
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-13 19:35:30 +0000

    test

--- src/_01_Exercise/_00_chapter_06/Exercise6_5.java
+++ src/_01_Exercise/_00_chapter_06/Exercise6_5.java
@@ -18,6 +18,7 @@ class Exercise6_5 {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(Arrays.toString(arr));
         
+        // 배열 섞기
         System.out.println(Arrays.toString(shuffle(arr)));
     }
 }
