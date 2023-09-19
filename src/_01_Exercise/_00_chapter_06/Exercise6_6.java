a50bf21bbf92b3df0a7e05795cb0220b8a0ea6cd
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-19 10:00:23 +0000

    chapter_06 Exercise - Exercise6_6.java

--- src/_01_Exercise/_00_chapter_06/Exercise6_6.java
+++ src/_01_Exercise/_00_chapter_06/Exercise6_6.java
@@ -2,7 +2,7 @@ package _01_Exercise._00_chapter_06;
 
 public class Exercise6_6 {
     static boolean isNumber(String str) {
-        // str이 비어있는 경우 
+        // str이 null 이거나, 빈 문자열인 경우 
         if(str == null || str.equals("")){
             return false;
         }
@@ -19,7 +19,7 @@ public class Exercise6_6 {
     }
     
     public static void main(String[] args) {
-        String str = "1234";
+        String str = null;
         System.out.println(str + "은 숫자로만 이루어져 있는가? " + isNumber(str));
         
         str = "123o";
