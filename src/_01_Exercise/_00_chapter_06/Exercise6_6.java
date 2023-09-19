145833bcfc56e521473264ab32b5b1309579fe76
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-19 01:06:10 +0000

    chapter_06 Exercise - Exercise6_6.java

--- src/_01_Exercise/_00_chapter_06/Exercise6_6.java
+++ src/_01_Exercise/_00_chapter_06/Exercise6_6.java
@@ -2,25 +2,27 @@ package _01_Exercise._00_chapter_06;
 
 public class Exercise6_6 {
     static boolean isNumber(String str) {
-        // str이 비어있는 경우
-        if(str == null || str.equals("")) {
+        // str이 비어있는 경우 
+        if(str == null || str.equals("")){
             return false;
         }
         
+        // str이 숫자로만 이루어져 있는경우 / 아닌경우
         for(int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             
-            if(ch < '0' || ch > '9') {
+            if(ch < 48 || ch > 57) {
                 return false;
             }
         }
         return true;
     }
+    
     public static void main(String[] args) {
-        String str = "123";
-        System.out.println(str + "는 숫자입니까?" + isNumber(str));
+        String str = "1234";
+        System.out.println(str + "은 숫자로만 이루어져 있는가? " + isNumber(str));
         
         str = "123o";
-        System.out.println(str + "는 숫자입니까?" + isNumber(str));
+        System.out.println(str + "은 숫자로만 이루어져 있는가? " + isNumber(str));
     }
 }
