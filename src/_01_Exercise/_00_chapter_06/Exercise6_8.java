3463568967fc8d556160912439a183d873f5bc17
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-09-26 11:21:08 +0000

    chapter_06 Exercise - Exercise6_8.java

--- src/_01_Exercise/_00_chapter_06/Exercise6_8.java
+++ src/_01_Exercise/_00_chapter_06/Exercise6_8.java
@@ -2,22 +2,17 @@ package _01_Exercise._00_chapter_06;
 
 public class Exercise6_8 {
     static int max(int[] arr) {
-        if(arr != null || arr.length != 0) {
-            int max_index = 0;
-            
-            for(int i = 1; i < arr.length; i++) {
-                if(arr[max_index] < arr[i]) {
-                    max_index = i;
-                }
-            }
-            
-            return arr[max_index];
-        }
-        else {
+        if(arr == null || arr.length == 0) {
             return -999999;
         }
+        
+        int max_value = arr[0];
+        
+        for(int i = 1; i < arr.length; i++) {
+            max_value = (max_value < arr[i])? max_value : arr[i];
+        }
+        return max_value;  
     }
-    
     public static void main(String[] args) {
         int[] data = {3, 2, 9, 4, 7};
         System.out.println(java.util.Arrays.toString(data));
