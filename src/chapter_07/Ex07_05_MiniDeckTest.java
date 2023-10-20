bd7ca84c9b36253ca03b75b72626696050f88cc0
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-10-20 10:55:05 +0000

    inclusion relationships of java classes - Ex07_05_MiniDeckTest.java

---
+++ src/chapter_07/Ex07_05_MiniDeckTest.java
@@ -0,0 +1,34 @@
+package chapter_07;
+
+class M_Card {
+  int number;
+
+  M_Card(int number) {
+    this.number = number;
+  }
+}
+
+class M_Deck {
+  final static int CARD_MAX = 5;
+  M_Card[] cardArr = new M_Card[CARD_MAX];
+
+  M_Deck() {
+    for (int i = 0; i < CARD_MAX; i++) {
+      cardArr[i] = new M_Card(i + 1);
+    }
+  }
+
+  void show() {
+    for (int i = 0; i < cardArr.length; i++) {
+      System.out.println(cardArr[i].number);
+    }
+  }
+}
+
+class Ex07_05_MiniDeckTest {
+    public static void main(String[] args) {
+        M_Deck d = new M_Deck();
+        
+        d.show();
+    }
+}
