9ee06d98c22de7bf0fd584fa7b39e58424958e6d
Yoonsik-2002 <yoonsikp119105@gmail.com>
2023-12-13 10:37:07 +0000

    the creator's access modifier - Ex07_11_SingletonTest.java

---
+++ src/chapter_07/Ex07_11_SinlgetonTest.java
@@ -0,0 +1,24 @@
+package chapter_07;
+
+final class Singleton {
+    private static Singleton s = new Singleton();
+    
+    private Singleton() {
+        // ...
+    }
+    
+    public static getInstance() {
+        if(s == null) 
+            s = new Singleton();
+        
+        return s;
+    }
+}
+
+class Ex07_11_SingletonTest {
+    public static void main(String[] args) {
+        // Singleton s = new Singletion(); 
+        
+        Singleton s = Singleton.getInstance(); // public static 메서드이므로, 외부에서 클래스 이름을 통해 인스턴스를 생성하지 않아도 호출 가능
+    }
+}
