c4b58b298791b6285740958a3ef688c13bf14586
Yoonsik-2002 <yoonsikp119105@gmail.com>
2024-01-28 08:07:05 +0000

    chapter_07 Exercise - Exerecise7_11.java

--- src/_01_Exercise/_01_chapter_07/Exercise7_11.java
+++ src/_01_Exercise/_01_chapter_07/Exercise7_11.java
@@ -22,17 +22,17 @@ class DrawRobot extends Robot {
 }
 
 class Exercise7_11 {
-    void action(Robot r) {
-        if(r instanceof DanceRobot) {  // 해당 클래스 타입으로 형 변환이 가능한지 검사
-            r.dance();  // 가능한 경우, Robot -> DanceRobot으로의 다운 캐스팅(Down Casting)이므로, 형변환 생략 불가능
+    static void action(Robot r) {
+        if(r instanceof DanceRobot) {  // 참조변수 r의 타입을 DanceRobot 타입으로 형변환 할 수 있는지 검사
+            ((DanceRobot) r).dance();  // r의 타입을 Robot 에서 DanceRobot 타입으로 형변환(down-casting)
         }
         
-        else if(r instanceof SingRobot) {
-            r.sing();
+        if(r instanceof SingRobot) {
+            ((SingRobot) r).sing();
         }
         
-        else if(r instanceof DrawRobot) {
-            r.draw();
+        if(r instanceof DrawRobot) {
+            ((DrawRobot) r).draw();
         }
     }
     
@@ -48,6 +48,10 @@ class Exercise7_11 {
         
         for(int i = 0; i < array.length; i++) {
             action(array[i]);
+            /*
+            static 메서드인 main메서드에서의 직접적인 호출. 만약 action메서드가 인스턴스 메서드인 경우, 
+            main메서드 첫 줄에서 Exercise7_11 클래스의 인스턴스를 생성해 주어야 한다. 
+            */
         }
     }
 }
